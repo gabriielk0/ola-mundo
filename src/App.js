44a061a5345d5d9d1a825a6377e31308e9829f07
Gabriel <oliveiracgabriel052@gmail.com>
2024-09-02 20:18:38 -0300

    Adicionando Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./pages/inicio";
+import SobreMim from "./pages/SobreMim";
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/sobremim" element={<SobreMim/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
