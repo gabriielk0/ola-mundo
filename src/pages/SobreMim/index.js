54185de53f7481d5dcab7af8d9764221ec48fb56
Gabriel <oliveiracgabriel052@gmail.com>
2024-09-16 22:20:40 -0300

    Adicionando pagina sobremim

--- src/pages/SobreMim/index.js
+++ src/pages/SobreMim/index.js
@@ -1,9 +1,25 @@
+import styles from "./sobremim.module.css";
 
+import PostModelo from "PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.png";
 
 export default function SobreMim() {
-    return (
-        <main>
-            
-        </main>
-    )
+  return (
+    <main>
+      <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim" />
+      <h3 className={styles.subtitulo}>Olá, eu sou Gabriel</h3>
+
+      <img
+        src={fotoSobreMim}
+        alt="Foto do esquisito"
+        className={styles.fotoSobreMim}
+      />
+
+      <p className={styles.paragrafo}>Bla</p>
+      <p className={styles.paragrafo}>Bla</p>
+      <p className={styles.paragrafo}>Bla</p>
+      <p className={styles.paragrafo}>Bla</p>
+    </main>
+  );
 }
